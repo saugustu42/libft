92e46b5c33d43a4f63250912349554babb801e07
Александр Олешкевич <saugustu@ga-r5.msk.21-school.ru>
2020-11-07 16:42:04 +0300

    fixed memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: saugustu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 20:55:32 by saugustu          #+#    #+#             */
-/*   Updated: 2020/11/03 21:54:44 by saugustu         ###   ########.fr       */
+/*   Updated: 2020/11/07 16:38:28 by saugustu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,16 @@
 int						ft_memcmp(const void *buf1, const void *buf2, size_t n)
 {
 	int					diff;
-	const unsigned char	*s1;
-	const unsigned char *s2;
+	unsigned char		*s1;
+	unsigned char		*s2;
 
-	s1 = (const unsigned char*)buf1;
-	s2 = (const unsigned char*)buf2;
+	s1 = (unsigned char*)buf1;
+	s2 = (unsigned char*)buf2;
 	while (n--)
 	{
 		diff = *s1++ - *s2++;
-		if (diff < 0)
-			return (-1);
-		if (diff > 0)
-			return (1);
+		if (diff != 0)
+			return (diff);
 	}
 	return (0);
 }
